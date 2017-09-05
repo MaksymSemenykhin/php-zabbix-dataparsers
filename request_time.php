4f25adbd6d19e45ac6354cad9121a25a48d5c9c4
Maksym Semenykhin <MaksymSemenykhin@users.noreply.github.com>
2017-09-05 10:01:55 +0300

    Update request_time.php

--- request_time.php
+++ request_time.php
@@ -5,13 +5,13 @@ UserParameter=couchdb-request_time[*], php -f /etc/zabbix/scripts/request_time.p
 */
 
 //key in json data
-$argv[1]
+$argv[1];
 //host to connect to
-$argv[2]
+$argv[2];
 
 
 $request_time = file_get_contents($argv[2].'/_stats/couchdb/request_time');
 
-$request_time = json_decode($request_time);
+$request_time = json_decode($request_time,true);
 
 echo $request_time['couchdb']['request_time'][$argv[1]];
